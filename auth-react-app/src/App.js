78704b59481dbe7b87ad3709a7ccb191daf167b8
umiigo <dev.umigo@gmail.com>
2019-05-12 23:12:02 +0100

    completed auth

--- auth-react-app/src/App.js
+++ auth-react-app/src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import { Header } from "semantic-ui-react";
 
+import Amplify from "aws-amplify";
+import aws_exports from "./aws-exports";
+import { withAuthenticator } from "aws-amplify-react";
+Amplify.configure(aws_exports);
+
 class App extends Component {
   render() {
     return (
@@ -11,4 +16,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true });
